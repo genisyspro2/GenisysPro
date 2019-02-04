709641c843af541ed8a07f525e0f5f5c1312195a
BajanVlogs <kjolly544@gmail.com>
2019-02-04 07:02:50 -0500

    Update Info.php

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -30,10 +30,10 @@ interface Info {
 	 * Actual Minecraft: PE protocol version
 	 */
 
-	const CURRENT_PROTOCOL = 113;
-	const ACCEPTED_PROTOCOLS = [110, 111, 112, 113];
-	const MINECRAFT_VERSION = ["v1.1.0", "v1.1.1", "v1.1.2", "v1.1.3", "v1.1.4", "v1.1.5"];
-	const MINECRAFT_VERSION_NETWORK = "1.1.5";
+	const CURRENT_PROTOCOL = 313;
+	const ACCEPTED_PROTOCOLS = [310, 311, 312, 313];
+	const MINECRAFT_VERSION = ["v1.8.0"];
+	const MINECRAFT_VERSION_NETWORK = "1.8.0";
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
