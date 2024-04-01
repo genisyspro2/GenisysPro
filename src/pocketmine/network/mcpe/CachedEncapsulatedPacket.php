7ea4dfbde5f6f69f677dcf9ebcefaf9342d84846
BajanVlogs <kjolly544@gmail.com>
2024-04-01 13:17:51 -0400

    Create CachedEncapsulatedPacket.php

---
+++ src/pocketmine/network/mcpe/CachedEncapsulatedPacket.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace pocketmine\network\mcpe;
+
+use raklib\protocol\EncapsulatedPacket;
+
+class CachedEncapsulatedPacket extends EncapsulatedPacket{
+	/** @var string|null */
+	private $internalData = null;
+
+	public function toInternalBinary() : string{
+		return $this->internalData ?? ($this->internalData = parent::toInternalBinary());
+	}
+}
